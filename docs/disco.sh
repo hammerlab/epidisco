d86189f95815f93ae8ba0ae46a62246c89427952
Isaac Hodes <isaachodes@gmail.com>
2016-09-09 18:00:39 -0400

    Add docs and quickrun script
    This should make it quick and easy to get MVP Coclobas cluster up and
    running, as well as submit an Epidisco workflow to it.

---
+++ docs/disco.sh
@@ -0,0 +1,200 @@
+trap 'exit' ERR
+
+export GCLOUD_ZONE="us-east1-c"
+
+if [[ "$#" == "0" ]]; then
+    cat <<EOF
+USAGE
+
+$0 is used to easily set up a GCloud box with some default settings.
+
+You must have the gcloud CLI tool installed and configured.
+EOF
+    exit 1
+fi
+
+create () {
+    local boxname="$1"; shift
+    local path="`dirname \"$0\"`"
+    local path="`( cd \"$path\" && pwd )`"
+    local script="$0"
+    gcloud compute instances create $boxname \
+           --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud \
+           --zone us-east1-c --scopes cloud-platform --boot-disk-size 40GB
+    gcloud compute firewall-rules create https-on-$boxname --allow tcp:443 \
+           --source-tags=$boxname --source-ranges 0.0.0.0/0
+    # ##*/ gets the basename of "script"
+    gcloud compute copy-files $path/${script##*/} $boxname:~
+}
+
+get-external-ip () {
+    local boxname=$(hostname)
+    local desc="gcloud compute instances describe $boxname"
+    local ip=$($desc | grep natIP | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+    echo $ip
+}
+
+configure () {
+    # TODO
+    # 1. GATK/MUTECT/NETMHC URLS
+    # 2. NFS mounts
+    local boxname=$(hostname)
+    local nfsserver=$boxname-nfs
+    local externalip=$(get-external-ip)
+    cat <<EOF > configuration.env
+## Template for Configuring Ketrew with Coclobas scripts
+
+## Set the name of the cluster you want to manage:
+export CLUSTER_NAME=$boxname-cluster
+export NFS_SERVER_NAME=$nfsserver
+
+export EXTERNAL_IP=$externalip
+
+## Set the gcloud zone
+export GCLOUD_ZONE=$GCLOUD_ZONE
+
+## Choose an authentication token for the Ketrew server:
+#
+export TOKEN=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+## Number of compute nodes in the deployed cluster:
+#
+export CLUSTER_MAX_NODES=15
+
+## Description of the NFS services that we want the containers to mount
+##
+## A :-separated list of ,-separated 4-tuples
+##
+## Each tuple is:  <nfs-server-vm>,<nfs-remote-path>,<witnessfile>,<mount-point>
+##
+## - <nfs-server-vm> is the instance name
+## - <nfs-remote-path> is the path on the server that we want to mount
+## - <witness-file> is a relative path to a file that should exist on the mount (used for verification)
+## - <mount-point> is the path where we mount the NFS service (on all cluster nodes)
+
+export NFS_MOUNTS=$nfsserver,/nfs-pool/,.witness.txt,/nfs-pool
+
+## Variables used in the biokepi_machine.ml script:
+
+##  Biokepi configuration requires a few directories shared accross the cluster:
+## $BIOKEPI_WORK_DIR is mandatory;
+export BIOKEPI_WORK_DIR=/nfs-pool/biokepi/
+
+# Optional
+## INSTALL_TOOLS_PATH is optional (default is $BIOKEPI_WORK_DIR/toolkit):
+# export INSTALL_TOOLS_PATH=/nfs-constants/biokepi-software/
+## PYENSEMBL_CACHE_DIR is optional (default is $BIOKEPI_WORK_DIR/pyensembl-cache):
+# export PYENSEMBL_CACHE_DIR=/nfs-constants/biokepi-pyensemble-cache/
+## REFERENCE_GENOMES_PATH is optional (default is $BIOKEPI_WORK_DIR/reference-genome)
+# export REFERENCE_GENOMES_PATH=/nfs-constants/biokepi-ref-genomes/
+## ALLOW_DAEMONIZE is optional (default: false)
+## if true some nodes (such as downloads or moving data around) will run with
+## daemonize backend (i.e. on the server/docker container).
+# export ALLOW_DAEMONIZE=true
+
+## DOCKER_IMAGE is optional (default: hammerlab/biokepi-run):
+## The docker image to use for the Kubernetes jobs
+# export DOCKER_IMAGE=something/this:that
+
+## Usual Biokepi variables used to download Broad software:
+# export GATK_JAR_URL="http://example.com/GATK.jar"
+# export MUTECT_JAR_URL="http://example.com/Mutect.jar"
+EOF
+    gcloud config set compute/zone $GCLOUD_ZONE
+}
+
+# meant to be sudo-executed
+setup-docker () {
+    apt-get install -y docker.io
+    docker pull hammerlab/coclobas
+    mkdir -p /tmp/coclo
+    chmod 777 /tmp/coclo
+    cp configuration.env /tmp/coclo
+    cp $0 /tmp/coclo
+}
+
+# meant to be sudo-executed
+# - `--privileged` is for NFS mounting
+# - `-p 443:443` is to pass the port 443 to the container
+enter-docker () {
+    docker run -it -p 443:443 -v /tmp/coclo:/coclo \
+           --privileged hammerlab/coclobas bash
+}
+
+###########################
+# Inside the Docker image #
+###########################
+
+cluster-status () {=
+    echo $(curl http://localhost:8082/status)
+}
+
+install-epidisco () {
+    opam pin add --yes biokepi "https://github.com/hammerlab/biokepi.git"
+    opam pin add --yes epidisco "https://github.com/hammerlab/epidisco.git"
+}
+
+create-nfs () {
+    local size=$1
+    local project=$(curl "http://metadata.google.internal/computeMetadata/v1/project/project-id" \
+                         -H "Metadata-Flavor: Google" 2>/dev/null)
+    source /coclo/configuration.env
+    # This utility (https://github.com/cioc/gcloudnfs) is pre-installed on the
+    # Docker image.
+    gcloudnfs create --zone us-east1-c --project $project \
+              --network default --machine-type n1-standard-1 \
+              --server-name $NFS_SERVER_NAME \
+              --data-disk-name $NFS_SERVER_NAME-disk --data-disk-type pd-standard \
+              --data-disk-size $size
+    sudo mkdir -p /nfs-pool
+    sudo mount -t nfs $NFS_SERVER_NAME:/nfs-pool /nfs-pool
+    touch /nfs-pool/.witness.txt
+}
+
+create-pipeline-script () {
+    cat <<EOF > run_pipeline.ml
+#use "topfind";;
+#thread
+#require "epidisco";;
+
+#use "biokepi_machine.ml";;
+
+let () =
+  Epidisco.Command_line.main ~biokepi_machine ()
+
+EOF
+    echo "Created ./run_pipeline.ml"
+
+}
+
+setup-epidisco () {
+    install-epidisco
+    create-pipeline-script
+    wget https://raw.githubusercontent.com/hammerlab/coclobas/master/tools/docker/biokepi_machine.ml
+    source configuration.env
+    ketrew init --conf ./_kclient_config/ --just-client https://$EXTERNAL_IP/gui?token=$TOKEN
+}
+
+submit-test () {
+    source configuration.env
+    export DREAM=https://storage.googleapis.com/dream-challenge
+    KETREW_CONFIGURATION=_kclient_config/configuration.ml \
+        ocaml run_pipeline.ml pipeline \
+          --normal $DREAM/synthetic.challenge.set2.normal.bam \
+          --tumor  $DREAM/synthetic.challenge.set2.tumor.bam \
+          --reference-build b37 \
+          --results-path $BIOKEPI_WORK_DIR/results/ \
+          -E My-first-epidisco-party
+}
+
+start-all () {
+    # This script comes on the Docker image
+    please.sh /coclo/configuration.env start_all
+}
+
+ketrew-ui () {
+    source configuration.env
+    echo https://$EXTERNAL_IP/gui?token=$TOKEN
+}
+
+$*
---
+++ docs/running-on-coclobas.md
@@ -0,0 +1,131 @@
+# Running Epidisco With Ketrew/Coclobas
+
+## Setting Things Up
+
+This quickstart guide will get you running Epidisco on test data (or your own)
+on the GCloud platform, using our Kubernetes scheduler, Coclobas.
+
+To keep things as simple as possible, we use a script (`please.sh`) to do most
+of the work. If you're interested in what's going on behind the scenes, please
+check out the script, which is documented, or you can always try `./please.sh
+type <command>` to see what a particular command is doing, and just
+`./please.sh` to get a list of all possible commands.
+
+### Set Up a Launch Box
+
+We need a server on GCloud to run our scheduler and Ketrew, our workflow
+engine. This box will serve as the interface between your workflow and GCloud's running
+of them.
+
+You'll need the GCloud command line utility, installed
+[from here](https://cloud.google.com/sdk/downloads#interactive).
+
+Download our setup script and use it to create a GCloud box:
+
+```shell
+wget https://...
+chmod 777 disco.sh
+./disco.sh create example-box-name
+```
+
+Once it's created, you will be able to ssh in with `gcloud compute ssh
+example-box-name`. The rest of the instructions will be carried out inside this
+VM.
+
+
+### Configuring the Cluster
+
+We will now create an NFS filer that the cluster will use, and generate a
+configuration file for both a Coclobas cluster and, to be used later, a Biokepi
+machine. Note that `disco.sh` should have been copied to your home directory on
+this box for you already.
+
+```shell
+./disco.sh configure
+```
+
+TODO: how to get GATK_JAR_URL. MUTECT_JAR_URL, NET_MHC_jars...
+TODO: explain what's going on, and that the config file is editable by hand
+
+Next we want to set up and run our Docker image, which is where Coclobas and
+Ketrew will be running, and eventually is where we will submit our workflows
+from.
+
+```shell
+sudo bash ./disco.sh setup-docker
+sudo bash ./disco.sh enter-docker
+```
+
+### Inside the Docker
+
+```shell
+cd /coclo
+./disco.sh create-nfs 1000  # size in GB
+./disco.sh start-all
+```
+
+You should now be in a screen session with four windows.
+
+- `Ketrew-server`
+- `Coclobas-server`
+- `Sudo-tlstunnel`
+- `bash`
+
+You will be in the `bash` window.
+
+The first time the Coclobas server is started, it needs to create a the
+Kubernetes cluster. This may take about 5-10 minutes; you can check its status
+with `./disco.sh cluster-status`.
+
+If it says `Initializing` wait a bit and try again, if it says `Ready`, we can
+go on to the next step.
+
+You can visit the Google Container Engine WebUI watch your cluster being
+created in the "Instance Group" tab.
+
+#### Note About Letting the Servers Live
+
+If you need to log out of the GCloud box, you want to leave the Docker container
+running. There are two ways to accomplish this.
+
+1. You can use `Ctrl-p Ctrl-q` to detach from Docker (and then `sudo docker ps`
+   / `sudo docker attach <id>` to reattach).
+2. You can run erverything inside tmux (just start tmux before running `./disco.sh enter-docker`.
+
+## Running Epidisco
+
+### Installing Epidisco
+
+We'll need to install and configure Epidisco, done with:
+
+```shell
+./disco.sh setup-epidisco
+```
+
+Now we're ready to run Epidisco on our new elastic cluster.
+
+You can now submit a job running on example data with the below invocation (also
+done with `./disco.sh submit-test`).
+
+```shell
+source configuration.env
+KETREW_CONFIGURATION=_kclient_config/configuration.ml \
+    DREAM=https://storage.googleapis.com/dream-challenge \
+    ocaml run_pipeline.ml pipeline \
+       --normal $DREAM/synthetic.challenge.set2.normal.bam \
+       --tumor $DREAM/synthetic.challenge.set2.tumor.bam \
+       --reference-build b37 \
+       --results-path $BIOKEPI_WORK_DIR/results/ \
+       -E My-first-epidisco-party
+```
+
+If you'd like to submit with your own data, you can inspect the options of
+ Epidisco with `ocaml run_pipeline.ml pipeline --help`.
+
+You can watch the run execute through the Ketrew Web UI found at `./disco.sh
+ketrew-ui`; the result of the run can be found in `$BIOKEPI_WORK_DIR/results`.
+
+
+## Shutting it all down
+
+TODO

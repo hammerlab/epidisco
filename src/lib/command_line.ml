cd23e9f37fb82febd449e2bd339c590b80cb583d
Isaac Hodes <isaachodes@gmail.com>
2016-10-14 13:58:08 -0400

    Add mouse option back
    Don't know how it got deleted...

--- src/lib/command_line.ml
+++ src/lib/command_line.ml
@@ -148,6 +148,7 @@ let pipeline_term
       in
       pure begin fun
         (`Dry_run dry_run)
+        (`Mouse_run mouse_run)
         (`With_seq2hla with_seq2hla)
         (`With_mutect2 with_mutect2)
         (`With_varscan with_varscan)
@@ -192,7 +193,7 @@ let pipeline_term
           in
           let params =
             Pipeline.Parameters.make experiment_name
-              ~bedfile
+              ~bedfile ~mouse_run
               ~reference_build ~normal ~tumor ?rna
               ?mhc_alleles
               ?email_options
@@ -209,6 +210,13 @@ let pipeline_term
         pure (fun b -> `Dry_run b)
         $ Arg.(
             value & flag & info ["dry-run"] ~doc:"Dry-run; do not submit"
+          )
+      end
+      $ begin
+        pure (fun b -> `Mouse_run b)
+        $ Arg.(
+            value & flag & info ["mouse-run"]
+              ~doc:"Mouse-run; use mouse-specific config (no COSMIC)"
           )
       end
       $ tool_option (fun e -> `With_seq2hla e) "seq2hla"
